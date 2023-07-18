9384bb7bb37a209b0de6d8af711a414e242d57c6
tanukesensei <luanvs.hack@gmail.com>
2023-07-18 17:35:21 -0300

    fix the home icon

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => (
   <Section>
     <NavbarTwoColumns>
       <a href="/">
-        <Logo
+        {/* <Logo
           icon={
             <svg
               className="mr-1 h-10 w-10 stroke-cyan-600"
@@ -20,6 +20,25 @@ const Navbar = () => (
               <rect x="9" y="8" width="6" height="12" rx="1"></rect>
               <rect x="15" y="4" width="6" height="16" rx="1"></rect>
               <path d="M4 20h14"></path>
+            </svg>
+          }
+          name="Home"
+        /> */}
+        <Logo
+          icon={
+            <svg
+              className="mr-1 h-10 w-10 stroke-cyan-600"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 256 256"
+            >
+              <path
+                d="M152,208V160a8,8,0,0,0-8-8H112a8,8,0,0,0-8,8v48a8,8,0,0,1-8,8H48a8,8,0,0,1-8-8V115.54a8,8,0,0,1,2.62-5.92l80-75.54a8,8,0,0,1,10.77,0l80,75.54a8,8,0,0,1,2.62,5.92V208a8,8,0,0,1-8,8H160A8,8,0,0,1,152,208Z"
+                fill="#0F7FCF"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+              />
             </svg>
           }
           name="Home"
